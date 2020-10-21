cd23c2088a23aec0934287dff3e6156397382a3e
jselinger <jsel2008@witxp.com>
2020-10-20 22:34:16 -0400

    Regex Escape special chars

--- JS_PasteKeyboard/JS_KeyModule.vb
+++ JS_PasteKeyboard/JS_KeyModule.vb
@@ -1,8 +1,9 @@
-﻿Module JS_KeyModule
+﻿Imports System.Text.RegularExpressions
+Module JS_KeyModule
     Public Title1 As String
     Public counter1 As Int16 = 10
     Public Function Sendkey(TexttoType As String)
-
+        TexttoType = Regex.Replace(TexttoType, "[+^%~()\\{\\}]", "{$0}")  'escape special characters for sendkey
         My.Computer.Keyboard.SendKeys(TexttoType, True)
         Return (0)
     End Function
